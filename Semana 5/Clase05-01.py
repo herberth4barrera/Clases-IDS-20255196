0d899c43b7d2038d11a3620efecae5f7e2a7dbb9
herberth4barrera <20255196@esen.edu.sv>
2025-10-07 17:14:09 -0600

    Clase 05

---
+++ Semana 5/Clase05-01.py
@@ -0,0 +1,27 @@
+nombre = "Herberth"
+palabra = "RECONOCER"
+otra_palabra = "palabra"
+
+
+print(len(nombre))
+
+print(nombre[2])
+
+print(nombre[2:5[)
+
+print(len(palabra))
+print(palabra[4:9[)
+
+print(palabra[0::2]) #consonantes de la variable palabra
+
+print(palabra[1::3]) #vocales de la varibale palabra 
+
+print(palabra[::]) #Salto por defecto por cada una de las letras de la variable
+
+print(palabra[::-1]) #Saltos al reves
+
+print(otra_palabra[::-1])
+
+palabra = "sorbete"
+
+print(palabra==palabra[::-1]) 
