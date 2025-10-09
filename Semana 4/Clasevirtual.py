046abc5d92dd31dbc3199cb83fb7fa2bcbcbbc3a
Herberth Barrera <20255196@esen.edu.sv>
2025-10-09 14:22:17 -0600

    Clase virtual 1

---
+++ Semana 4/Clasevirtual.py
@@ -0,0 +1,50 @@
+"""usuario = "Herberth" #Tipo string
+
+#Validar el usuario
+
+
+#Mostrar menus segun usuario
+
+#Definir tareas para el uduario
+
+#Presentar resumenes del usuario
+
+
+cantidad_alumnos = 74 #variable enteros int
+media_edad = 18.231234  #variable float 
+hope_fondo = 34567.234
+inversion = -9654.21548"""
+
+"""print(type(cantidad_alumnos))
+print(type(media_edad))
+
+print(type(cantidad_alumnos)is int)
+print(type(media_edad)is int)"""
+
+"""print("El ususario es",usuario, "y tiene", media_edad,"años", "y tiene", cantidad_alumnos, "estudiantes")
+
+print(f"El usuario es {usuario}")
+print(f"y en su aula con {cantidad_alumnos-4} estudiantes")
+
+print(f"con edad promedio de {media_edad:.2f} años")
+print(f"colectaron ${hope_fondo:,.2f} dolares")
+print(f"y la totalidad de gastos fue de ${abs(inversion):,.2f} dolares")
+
+print(type(usuario) is str)
+
+esta_lloviendo = False
+print(type(esta_lloviendo) is not bool)
+print(type(hope_fondo) is not bool)"""
+
+"""nombre = "Herberth"
+apellido = "Barrera"
+nombre_completo = nombre + " " +apellido
+print(nombre_completo)"""
+
+cantidad_alumnos = 25
+nombre_profe = "Herberth"
+nuevas_inscripciones = 0
+nombre_profe = input("Ingrese el nombre del profesor: ")
+nuevas_inscripciones2 = int(input("Nuevos alumnos: "))
+print(type(nombre_profe))
+print(nombre_profe + nuevas_inscripciones2)
--- Semana 5/Clase05-01-1.py
+++ Semana 5/Clase05-01-1.py
@@ -1,16 +1,16 @@
 ###Ejercicios sobre tuplas
 
- tupla1 = (1,12,255,1289,60000)
- lista1 = [1,12,255,1289,60000]    #La lista es mutable
- print(len(tupla1))
- print(tupla1[3:])
+tupla1 = (1,12,255,1289,60000)
+lista1 = [1,12,255,1289,60000]    #La lista es mutable
+print(len(tupla1))
+print(tupla1[3:])
 
 
- tupla1[-1] = 3
- print(tupla1)      #La tupla es inmutable
+tupla1[-1] = 3
+print(tupla1)      #La tupla es inmutable
 
 lista1[-1] = 3
 
 print(lista1)
 
-lista1.
+lista1.index
