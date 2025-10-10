1e71a42a9d9dca078539c564099f03a19e35eb78
Herberth Barrera <20255196@esen.edu.sv>
2025-10-09 22:02:43 -0600

    ejercicios con python

---
+++ Ejercicios básicos/Ejercicios.py
@@ -0,0 +1,43 @@
+"""nombre = "Herberth"
+edad = "20 años"
+ciudad = "Los Planes de Renderos"
+profesion = "Ingeniero de la nube"
+print(nombre+ " de "+edad+ " de "+ciudad+ " e "+profesion)"""
+
+"""mensaje = "Bienvenido al curso de Python"
+print(mensaje)"""
+
+"""año_actual = 2025
+año_nacimiento = 2005
+print(f"El usuario tiene {año_actual-año_nacimiento} años")"""
+
+"""pi = 3.14159265359
+print(f"El valor aproximado de pi es {pi:.4f}")"""
+
+"""palabra = "Python"
+print(palabra[0])
+print(palabra[0:3])
+print(palabra[4:6])"""
+
+""""frase = "Aprender Python es divertido"
+print(frase[9:15])"""
+
+"""palabra = input("Ingrese una palabra: ")
+#print(palabra.__len__())
+
+print(palabra[0])
+print(palabra[-1]) """
+
+"""codigo = "ABC1234XYZ"
+print(codigo[3:7])"""
+
+"""nombre = "Juan Perez"
+nombre.split()
+print(nombre.split()[-1])"""
+
+"""texto = "banana"
+print(texto.count("a"))"""
+
+mensaje = "El Salvador es un gran país"
+posicion_mensaje = mensaje.index("gran")
+print(posicion_mensaje)
---
+++ Semana 4/Clasevirtual2.py
@@ -0,0 +1,13 @@
+
+"""ItemIngresar = input("Ingrese el item: ")
+print(f"El item ingresado es {ItemIngresar}")"""
+
+my_string = "ABCDEFGHI"  #string, cadena de caracteres
+#letra = int(input("Indique el indice de la letra a mostrar (del 0 al 4): "))
+#print(len(my_string))
+#print(my_string[1:5:50])
+
+#print(min(my_string))
+#print(max(my_string))
+
+print(my_string.count("C"))
