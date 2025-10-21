71f9e9ae1ad848c40e42ec1a79fa4e19d19fc1db
Herberth Barrera <20255196@esen.edu.sv>
2025-10-21 16:59:34 -0600

    Create clase21-10-25.py

---
+++ Clases/clase21-10-25.py
@@ -0,0 +1,9 @@
+#Realizamos los ejercicios del parcial, para un mayor entendimiento de los conceptos
+
+#Ejercicio 1
+
+num1 = input()
+num2 = input()
+
+son_iguales = num1 == num2
+print(son_iguales)
