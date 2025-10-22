d637c96d0faef9617ca3fe3c2925e3e4f73a6fb6
Herberth Barrera <20255196@esen.edu.sv>
2025-10-22 08:50:09 -0600

    Update clase21-10-25.py

--- Clases/clase21-10-25.py
+++ Clases/clase21-10-25.py
@@ -2,8 +2,29 @@
 
 #Ejercicio 1
 
-num1 = input()
+"""num1 = input()
 num2 = input()
 
 son_iguales = num1 == num2
-print(son_iguales)
+print(son_iguales)"""
+
+
+#Listas
+
+datos = [1,2,"tres", ["lunes","martes","miercoles"]]
+#print(datos[2][1])
+print(datos[-1][-1][3])
+
+numero = ["uno", "dos", "tres"]
+print(numero)
+numeros = numero + ["cuatro", "cinco", "seis"]
+print (numeros)
+print(len(numeros))
+
+numeros[2] = "trois"
+print(numeros)
+
+numeros.append(input("Ingrese un numero: Escriba el siguiente numero: "))
+print(numeros)
+numeros.insert(2, input("Einender nummer:"))
+print(numeros)
