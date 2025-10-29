51042a10a2fa51913f9af09aa07fe8b7e907090c
Herberth Barrera <20255196@esen.edu.sv>
2025-10-29 17:38:48 -0600

    Update clase29-10-25.py

--- Clases/clase29-10-25.py
+++ Clases/clase29-10-25.py
@@ -2,17 +2,17 @@
 
 ejecucion = True
 
-while ejecucion:
+"""while ejecucion:
     opcion = input("Estamos ejecctando el menu? Y/N:")
     if opcion.lower() == "n":
         ejecucion = False
     elif opcion.lower() == "y":
         print("La opcion elegida no es valida")
         
-print("Gracias por usar nuestro sistema")
+print("Gracias por usar nuestro sistema")"""
 
 #Ejecicio 2
-alumnos = 0
+"""alumnos = 0
 lista_alumnos = []
 
 alumno = input("Digite nombre" )
@@ -22,4 +22,18 @@ lista_alumnos.append(alumno)
 alumno = input("Digite nombre" )
 lista_alumnos.append(alumno)
 
-print(alumnos)
+print(alumnos)"""
+
+#Otra opcion
+alumno = 0
+lista_alumnos = []
+cantidad = int(input("Ingrese la cantidad de alumnos: "))
+
+for i in range(cantidad):
+    alumno = input("Ingrese nombre: ")
+    lista_alumnos.append(alumno)
+    print(lista_alumnos)
+    
+alumno = input("Ingrese nombre: ")
+lista_alumnos.append(alumno)
+print(alumno)
